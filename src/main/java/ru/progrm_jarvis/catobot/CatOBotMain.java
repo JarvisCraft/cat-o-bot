1bbae9700ea7b3a66f458dc512d7520191e4146b
JarvisCraft <mrjarviscraft@gmail.com>
2019-05-08 15:40:51 +0300

    Add main class file

---
+++ src/main/java/ru/progrm_jarvis/catobot/CatOBotMain.java
@@ -0,0 +1,13 @@
+package ru.progrm_jarvis.catobot;
+
+public class CatOBotMain implements Runnable {
+
+    public static void main(final String... args) {
+        new CatOBotMain().run();
+    }
+
+    @Override
+    public void run() {
+        // TODO: 08.05.2019 app logic
+    }
+}
