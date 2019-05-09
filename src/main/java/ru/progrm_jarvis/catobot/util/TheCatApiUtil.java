40c72e9e55eb199855dbbb0838833b552ecda485
JarvisCraft <mrjarviscraft@gmail.com>
2019-05-09 23:50:25 +0300

    Remove explicit `static` modifiers from TheCatApiUtil

--- src/main/java/ru/progrm_jarvis/catobot/util/TheCatApiUtil.java
+++ src/main/java/ru/progrm_jarvis/catobot/util/TheCatApiUtil.java
@@ -24,18 +24,17 @@ public class TheCatApiUtil {
     /**
      * {@link Gson GSON} instance used for mapping TheCatApi JSON objects
      */
-    private static final Gson GSON = new Gson();
+    private final Gson GSON = new Gson();
 
     /**
      * Minimal amount of cats returned
      */
-    public static final int MIN_CATS_PER_REQUEST = 1, MAX_CATS_PER_REQUEST = 100;
+    public final int MIN_CATS_PER_REQUEST = 1, MAX_CATS_PER_REQUEST = 100;
 
     /**
      * Endpoint for HTTP-request for getting random cat images
      */
-    public static final URI RANDOM_CAT_IMAGE_REQUEST_ENDPOINT
-            = URI.create("https://api.thecatapi.com/v1/images/search");
+    public final URI RANDOM_CAT_IMAGE_REQUEST_ENDPOINT = URI.create("https://api.thecatapi.com/v1/images/search");
 
     /**
      * Parsers the content of the specified data stream as a JSON-array of {@link TheCatApiCatImage}s.
