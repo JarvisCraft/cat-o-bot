f0232fa7e5beefcdaa5af6248fc86078799d3a73
JarvisCraft <mrjarviscraft@gmail.com>
2019-05-09 23:14:40 +0300

    Add CatImageRepository

---
+++ src/main/java/ru/progrm_jarvis/catobot/image/repository/CatImageRepository.java
@@ -0,0 +1,21 @@
+package ru.progrm_jarvis.catobot.image.repository;
+
+import java.util.Optional;
+import java.util.concurrent.Future;
+
+/**
+ * Repository of cat images responsible for their storage and effective collecting.
+ *
+ * @param <I> type of cat images stored
+ * @param <C> type of configuration required to work with cat images or {@link Void} if none is expected
+ */
+public interface CatImageRepository<I, C> {
+
+    /**
+     * Packs a random cat image from this repository.
+     *
+     * @param configuration configurations used to get a cat image
+     * @return future returning {@link Optional} which should contain a cat image or be empty if something goes wrong
+     */
+    Future<Optional<I>> pickRandomCatImage(final C configuration);
+}
