5e91be6de13dcdf7c1fcbec8945a61720b97d6e2
JarvisCraft <mrjarviscraft@gmail.com>
2019-05-09 22:57:53 +0300

    Add CollectionUtil

---
+++ src/main/java/ru/progrm_jarvis/catobot/util/CollectionUtil.java
@@ -0,0 +1,11 @@
+package ru.progrm_jarvis.catobot.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Utility for providing easier use of Java Collections API in specific cases.
+ */
+@UtilityClass
+public class CollectionUtil {
+
+}
