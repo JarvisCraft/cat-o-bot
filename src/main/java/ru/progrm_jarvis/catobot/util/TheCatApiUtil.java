91a8621ffd611cc2c872e87bdaab990ed2f17841
JarvisCraft <mrjarviscraft@gmail.com>
2019-05-09 23:20:29 +0300

    Add TheCatApiUtil

---
+++ src/main/java/ru/progrm_jarvis/catobot/util/TheCatApiUtil.java
@@ -0,0 +1,47 @@
+package ru.progrm_jarvis.catobot.util;
+
+import com.google.gson.Gson;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import lombok.val;
+import ru.progrm_jarvis.catobot.image.TheCatApiCatImage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+/**
+ * Utility for <a href="https://thecatapi.com">TheCatApi</a> stuff.
+ *
+ * @see <a href="https://docs.thecatapi.com/">docs of TheCatAPI</a>
+ */
+@UtilityClass
+public class TheCatApiUtil {
+
+    /**
+     * {@link Gson GSON} instance used for mapping TheCatApi JSON objects
+     */
+    private static final Gson GSON = new Gson();
+
+    /**
+     * Endpoint for HTTP-request for getting random cat images
+     */
+    public static final URI RANDOM_CAT_IMAGE_REQUEST_ENDPOINT
+            = URI.create("https://api.thecatapi.com/v1/images/search");
+
+    /**
+     * Parsers the content of the specified data stream as a JSON-array of {@link TheCatApiCatImage}s.
+     *
+     * @param dataStream data stream of JSON-array of cat images
+     * @return parsed cat image objects
+     */
+    @SneakyThrows(IOException.class)
+    public TheCatApiCatImage[] parseCatImages(@NonNull final InputStream dataStream) {
+        try (val reader = new BufferedReader(new InputStreamReader(dataStream))){
+            return GSON.fromJson(reader, TheCatApiCatImage[].class);
+        }
+    }
+}
