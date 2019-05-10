11fb31c2037437e55b23f8c6238676ca83ed2f36
JarvisCraft <mrjarviscraft@gmail.com>
2019-05-10 18:48:00 +0300

    Add CatImageRepository#pickRandomCatImages(..)

--- src/main/java/ru/progrm_jarvis/catobot/image/repository/CatImageRepository.java
+++ src/main/java/ru/progrm_jarvis/catobot/image/repository/CatImageRepository.java
@@ -1,7 +1,11 @@
 package ru.progrm_jarvis.catobot.image.repository;
 
+import lombok.val;
+import lombok.var;
 import ru.progrm_jarvis.catobot.image.CatImage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Future;
 
@@ -20,6 +24,24 @@ public interface CatImageRepository<I extends CatImage, C> extends AutoCloseable
      * @return future returning {@link Optional} which should contain a cat image or be empty if something goes wrong
      */
     Future<Optional<I>> pickRandomCatImage(C configuration);
+
+    /**
+     * Packs random cat images from this repository.
+     *
+     * @param count amount of cat images to get
+     * @param configuration configurations used to get a cat image
+     * @return list of futures containing optional {@link CatImage}s
+     *
+     * @throws IllegalArgumentException if count is negative
+     */
+    default List<Future<Optional<I>>> pickRandomCatImages(final int count, final C configuration) {
+        if (count < 0) throw new IllegalArgumentException("count should be non-negative");
+
+        val catImages = new ArrayList<Future<Optional<I>>>(count);
+        for (var i = 0; i < count; i++) catImages.add(pickRandomCatImage(configuration));
+
+        return catImages;
+    }
 
     @Override
     default void close() {}
