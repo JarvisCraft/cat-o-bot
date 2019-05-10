4b8f2e8ed559e92be3b64ee6862bb5262ee5c049
JarvisCraft <mrjarviscraft@gmail.com>
2019-05-10 20:28:23 +0300

    Remove redundant code in VkCatsManager

--- src/main/java/ru/progrm_jarvis/catobot/vk/VkCatsManager.java
+++ src/main/java/ru/progrm_jarvis/catobot/vk/VkCatsManager.java
@@ -3,10 +3,7 @@ package ru.progrm_jarvis.catobot.vk;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import lombok.NonNull;
-import lombok.SneakyThrows;
 import ru.progrm_jarvis.catobot.image.CatImage;
-
-import java.io.IOException;
 
 public interface VkCatsManager extends AutoCloseable {
     void startLongPolling();
@@ -15,7 +12,5 @@ public interface VkCatsManager extends AutoCloseable {
 
     void sendCatsUnavailable(int peerId);
 
-    @SneakyThrows(IOException.class)
-    void sendCatImages(int peerId, @NonNull CatImage... images)
-            throws ClientException, ApiException;
+    void sendCatImages(int peerId, @NonNull CatImage... images) throws ClientException, ApiException;
 }
