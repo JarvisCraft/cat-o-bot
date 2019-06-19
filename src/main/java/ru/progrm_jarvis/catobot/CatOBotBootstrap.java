2dc0986a095c9771a089dafa8d10dacc3aecf813
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-06-19 16:48:10 +0300

    Add broadcast support to CatOBotBootstrap

--- src/main/java/ru/progrm_jarvis/catobot/CatOBotBootstrap.java
+++ src/main/java/ru/progrm_jarvis/catobot/CatOBotBootstrap.java
@@ -11,12 +11,15 @@ import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 /**
  * Bootstrap of a standard Cat'o'Bot instance.
  */
 @Slf4j
 public class CatOBotBootstrap {
+
+    protected static final Pattern BROADCAST_COMMAND_PATTERN = Pattern.compile("(?:msg|message)\\s+(\\d+)+\\s(.*)");
 
     /**
      * Starts a Cat'o'Bot session which will attempt to restart it if exceptions occur while running.
@@ -51,20 +54,31 @@ public class CatOBotBootstrap {
                         // read line input
                         val line = cli.readLine();
                         // try to handle line input
+
+                        // critical commands
                         switch (line.toLowerCase()) {
                             case "reload": case "restart": continue botSession;
                             case "stop": case "end": break botSession;
+                        }
+
+                        { // <message|msg> <id> <message>
+                            val matcher = BROADCAST_COMMAND_PATTERN.matcher(line);
+                            if (matcher.matches()) {
+                                try {
+                                    bot.getVk().sendMessage(Integer.parseInt(matcher.group(1)), matcher.group(2));
+                                } catch (final Throwable e) {
+                                    log.warn("Unable to broadcast message", e);
+                                }
+                            }
                         }
                     } catch (final UserInterruptException e) { // handle < [CTRL] + [C] >
                         log.info("The session has been interrupted, aborting");
                         break botSession;
                     }
                 }
-            } catch (final CatOBot.BotInitializationException e) {
-                log.error("An exception occurred while initializing CatOBot, aborting", e);
-                break;
             } catch (final Throwable e) {
                 log.error("An exception occurred while running CatOBot, aborting", e);
+                break;
             }
         }
 
